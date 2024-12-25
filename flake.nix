1ad4e2a635992afa4584a1baadeb179df2dc94a9
3nyos <lovelypizzaparty@gmail.com>
2024-12-25 14:56:48 -0300

    update flake.nix

--- flake.nix
+++ flake.nix
@@ -1,43 +1,42 @@
 {
-  description = "Flake test, v2alpha";
-
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  };
-
-  outputs = inputs @ {
-    self,
-    nixpkgs,
-    ...
-  }: let
+  outputs = inputs @ {nixpkgs, ...}: let
     user = import ./user;
+
     forAllSystems = nixpkgs.lib.genAttrs [
       "aarch64-linux"
       "x86_64-linux"
       "x86_64-darwin"
       "aarch64-darwin"
     ];
-    filter = path: type: let
-      fileName = baseNameOf path;
-    in
-      !(builtins.hasSuffix ".nix" fileName || fileName == "flake.lock");
   in {
-    packages = forAllSystems (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in (user.packages pkgs
-      // {
-        src = builtins.filterSource filter ./.;
-      }));
-    formatter = forAllSystems (system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-      pkgs.alejandra);
+    packages = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in (user.packages pkgs)
+    );
+    # Formatter for your nix files, available through 'nix fmt'
+    formatter = forAllSystems (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+        pkgs.alejandra
+    );
+
     nixosModules =
       {
+        # This module is not meant to be imported by anyone but me
+        # it's just so I can easily avoid ../../../../../ mess
         system = import ./system;
+
         user = user.module;
+
       }
       // import ./modules;
+
     nixosConfigurations = import ./hosts inputs;
+  };
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
   };
 }
